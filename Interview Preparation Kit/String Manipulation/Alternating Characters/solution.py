7b1bc02e328652105c1d524d8c97dad8247394bc
Tejas M <36649349+TejM@users.noreply.github.com>
2019-01-03 19:23:31 -0600

    Create solution.py

---
+++ Interview Preparation Kit/String Manipulation/Alternating Characters/solution.py
@@ -0,0 +1,31 @@
+"""https://www.hackerrank.com/challenges/alternating-characters"""
+
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the alternatingCharacters function below.
+def alternatingCharacters(s):
+    numDelete = 0
+    for i in range (len(s) - 1, 0,-1):
+        if s[i] == s[i-1]:
+            numDelete += 1
+    return numDelete
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(raw_input())
+
+    for q_itr in xrange(q):
+        s = raw_input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
